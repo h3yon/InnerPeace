4c8eaa207310d49a86cc4a5b0b03e97d1f97fa1a
kangheeyoon <46602874+kangheeyoon@users.noreply.github.com>
2020-08-17 17:38:30 +0900

    8호선

--- java/Detail_Sub8_view.kt
+++ java/Detail_Sub8_view.kt
@@ -2,64 +2,72 @@ package com.project.innerpeace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_detail_sub_view.*
 
 class Detail_Sub8_view : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_sub_view)
-
+        setContentView(R.layout.activity_search_view)
         setSupportActionBar(findViewById(R.id.my_toolbar))
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         toolbar_title.setText("8 호선")
 
-        val sub8List = ArrayList<SubForList>()
+        val search = findViewById<SearchView>(R.id.searchView)
+        search.background = null
+        val listView = findViewById<ListView>(R.id.searchList)
 
-        sub8List.add(SubForList("암사"))
-        sub8List.add(SubForList("천호"))
-        sub8List.add(SubForList("강동구청"))
-        sub8List.add(SubForList("몽촌포성"))
-        sub8List.add(SubForList("잠실"))
-        sub8List.add(SubForList("석촌"))
-        sub8List.add(SubForList("송파"))
-        sub8List.add(SubForList("가락시장"))
-        sub8List.add(SubForList("문정"))
-        sub8List.add(SubForList("장지"))
-        sub8List.add(SubForList("복정"))
-        sub8List.add(SubForList("산성"))
-        sub8List.add(SubForList("남한산성 입구"))
-        sub8List.add(SubForList("단대오거리"))
-        sub8List.add(SubForList("신흥"))
-        sub8List.add(SubForList("수진"))
-        sub8List.add(SubForList("모란"))
+        val sub8List = arrayListOf<String>()
+        sub8List.add(("암사(외부)"))
+        sub8List.add(("천호(외부)_5,8"))
+        sub8List.add(("강동구청(외부)"))
+        sub8List.add(("몽촌포성(외부)"))
+        sub8List.add(("잠실(외부)_2,8"))
+        sub8List.add(("석촌(외부)_8,9"))
+        sub8List.add(("송파(외부)"))
+        sub8List.add(("가락시장(외부)_3,8"))
+        sub8List.add(("문정(외부)"))
+        sub8List.add(("장지(외부)"))
+        sub8List.add(("복정(외부)_8,분당"))
+        sub8List.add(("산성(외부)"))
+        sub8List.add(("남한산성 입구(외부)"))
+        sub8List.add(("단대오거리(외부)"))
+        sub8List.add(("신흥(외부)"))
+        sub8List.add(("수진(외부)"))
+        sub8List.add(("모란(외부)_8,분당"))
 
-        val adapter = DetailSubAdapter(sub8List, LayoutInflater.from(this@Detail_Sub8_view))
-        sub_recycler_view.adapter = adapter
-        sub_recycler_view.layoutManager = LinearLayoutManager(this@Detail_Sub8_view)
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        when (id) {
-            android.R.id.home -> {
-                finish()
-                return true
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
+            this, R.layout.simple_list_item_1, sub8List
+        )
+
+        listView.adapter = adapter
+//        names.setTextColor(Color.BLACK)
+
+        search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String): Boolean {
+                search.clearFocus()
+                if (sub8List.contains(p0)) {
+                    adapter.filter.filter(p0)
+                } else {
+                    Toast.makeText(applicationContext, "Item not found", Toast.LENGTH_LONG).show()
+                }
+                return false
             }
-        }
-        return super.onOptionsItemSelected(item)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu, menu)
-        return true
+            override fun onQueryTextChange(p0: String): Boolean {
+                adapter.filter.filter(p0)
+                return false
+            }
+
+        })
     }
 }
