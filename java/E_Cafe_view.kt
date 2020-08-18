4c2592edb0881134cbf68ec795d781169b32622e
whk06061 <63198157+whk06061@users.noreply.github.com>
2020-08-18 15:02:41 +0900

    Update E_Cafe_view.kt

--- java/E_Cafe_view.kt
+++ java/E_Cafe_view.kt
@@ -26,6 +26,9 @@ class E_Cafe_view : AppCompatActivity() {
 
         val manager = GridLayoutManager(this@E_Cafe_view, 2)
         
+        //      GPS_view에서의 사용자 입력 값을 inputString 변수에 전달받음
+        val inputString = intent.getStringExtra("input")
+        
         val itemList1_1 = listOf<Image>(
             Image(R.mipmap.one_1_1_cafe,
                 "카페 아를", "#남/녀 화장실 구분",
